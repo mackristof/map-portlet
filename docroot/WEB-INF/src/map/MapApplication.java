c913faeb4b20939ac4225240aadf49381f3acc1c
Christophe MOURETTE <christophe.mourette@geomatys.com>
2012-01-14 17:44:34 +0100

    initial revision

---
+++ docroot/WEB-INF/src/map/MapApplication.java
@@ -0,0 +1,24 @@
+package map;
+
+import com.vaadin.Application;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Window;
+
+public class MapApplication extends Application {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8695622759872012095L;
+
+	public void init() {
+		Window window = new Window();
+
+		setMainWindow(window);
+
+		Label label = new Label("Hello Map!");
+
+		window.addComponent(label);
+	}
+
+}
